8b26f631451a3ddb2d291aabfac93b398fada3af
jayjay-06 <tjhonjhon@gmail.com>
2023-04-05 16:46:21 +0800

    Edit script

--- script.js
+++ script.js
@@ -5,29 +5,38 @@ const lcdupper = document.getElementById('lcdscreenupper');
 const clearallinput =document.getElementById('CE');
 const clearonce =document.getElementById('C');
 const deletenum =document.getElementById('del');
-let numberarray=[];
+let lcdanswer = document.getElementById('lcdscreenlower');
 let screentext = false;
 let currentoperator = null;
 let firstnumber=0;
+let firsthasnumber = false
+let secondhasnumber=false
 let secondnumber=0;
 
 
 clearallinput.addEventListener('click',() => clearall());
 clearonce.addEventListener('click', () => clearone());
 deletenum.addEventListener('click', () => del());
+equals.addEventListener('click', () => operatenow(currentoperator,firstnumber,secondnumber))
 
 
 
 
 function inputed(num){
- if(lcdupper.textContent.length == 12 || lcdupper.textContent=='ERROR'){
-    lcdupper.textContent ='ERROR';
-    screentext=false;
- }
-else{
-        screentext=true;
-    lcdupper.textContent+=num;
-}
+
+if(lcdupper.textContent==='0' || screentext){
+        clearone();}
+         if(lcdupper.textContent.length == 12 || lcdupper.textContent=='ERROR'){
+            lcdupper.textContent ='ERROR';
+            screentext=false;
+         }
+         else {
+            lcdupper.textContent+=num;
+            if(firsthasnumber){
+            secondnumber=parseFloat(lcdupper.textContent);
+            secondhasnumber=true
+            }
+         }
 }
 
 function clearone() {
@@ -37,10 +46,16 @@ function clearone() {
 
 
 function clearall(){
-lcdupper.textContent=''
-screentext=false;
-currentoperator=null;
-numberarray=[];
+
+    lcdupper.textContent='';
+    lcdanswer.textContent='';
+    screentext=false;
+    currentoperator=null;
+    firstnumber=0;
+    firsthasnumber=false;
+    secondnumber=0;
+    secondhasnumber=false;
+
 }
 
 function del() {
@@ -48,11 +63,6 @@ function del() {
     lcdupper.textContent = lcdupper.textContent.toString().slice(0, -1);
     }
 }
-
-
-
-
-
 
 numberinput.forEach((button) =>{button.addEventListener('click',() => inputed(button.textContent));});
 
@@ -64,26 +74,47 @@ operator.forEach((button) =>{button.addEventListener('click',() => compute(butto
  */
 
 function compute(operate){
-    if(currentoperator!==operate){
-        currentoperator =operate;
+    if(currentoperator==null || firsthasnumber==false){
+        currentoperator = operate;
         firstnumber=parseFloat(lcdupper.textContent);
+        firsthasnumber=true;
+        screentext=true;
+    }
+    if(secondhasnumber){
+        currentoperator = operate;
+        lcdanswer.textContent=0;
+        firsthasnumber=false;
+        screentext=true;
+        secondhasnumber=false;
+        operatenow(currentoperator,firstnumber,secondnumber)
     }
 
+    }
+
+
+
+    function operatenow(operand, num1, num2){
+        switch(operand){
+            case'+':
+                return addition(num1,num2);
+            case'-':
+                return substact(num1, num2);
+            case'*':
+                return multiply(num1, num2);
+            default:
+                return divide(num1, num2);
+        }
+    }
+
+function addition(a,b){
+    return lcdanswer.textContent = a+b;
 }
-
-
-
-
-
-function addition(){
-
+function substact(a,b) {
+    return lcdanswer.textContent = a-b;
 }
-function substact() {
-
+function multiply(a,b) {
+    return lcdanswer.textContent = a*b;
 }
-function multiply() {
-
-}
-function divide() {
-
+function divide(a,b) {
+    return lcdanswer.textContent = a/b;
 }
